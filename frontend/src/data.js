10e359a44f5d80d12b958730c6ac6799ed5b3939
ilix <ilishjyoti17@gmail.com>
2021-09-18 16:05:57 +0530

    initial commit

---
+++ frontend/src/data.js
@@ -0,0 +1,34 @@
+export default {
+    products:[
+        {
+            _id: '1',
+            name: 'Lovey',
+            category: 'Hampers',
+            image: '/images/product-1.jpg',
+            price: 2000,
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+        },
+        {
+            _id: '2',
+            name: ' Dovey',
+            category: 'Hampers',
+            image: '/images/product-2.jpg',
+            price: 2000,
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+        },
+        {
+            _id: '3',
+            name: 'All izz well',
+            category: 'Hampers',
+            image: 'images/product-3.jpg',
+            price: 2000,
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+        },
+    ],
+};
